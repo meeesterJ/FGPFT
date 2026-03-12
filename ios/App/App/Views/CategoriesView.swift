e87b28e5b65666828f20d17e6bff82233d12004e
Keith Jasken <KJaskenAssoc@pm.me>
2026-03-12 17:10:26 -0500

    Refactor CategoriesView to correct toolbar background method usage, ensuring proper application of the navigation bar background color.

--- ios/App/App/Views/CategoriesView.swift
+++ ios/App/App/Views/CategoriesView.swift
@@ -73,7 +73,7 @@ struct CategoriesView: View {
         }
         .background(BackgroundView())
         .navigationBarTitleDisplayMode(.inline)
-        .toolbarBackground(.visible, AppColors.barBackground, for: .navigationBar)
+        .toolbarBackground(AppColors.barBackground, for: .navigationBar)
         .toolbar {
             ToolbarItem(placement: .principal) {
                 Text("Word Categories")
